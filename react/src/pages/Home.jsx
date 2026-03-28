0d2b33c6439ea15554b3c0e3e1e7cd8a02067c70
Eugene Martinez <eugenejrmartinez@gmail.com>
2026-03-29 01:13:30 +0800

    fixed component import reference

--- react/src/pages/Home.jsx
+++ react/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { ObservedSection } from "@components/context/ObservedSection";
 import Hero from "@components/sections/Hero";
 import BrandTicker from "@components/sections/BrandTicker";
 import SplitManifesto from "@components/sections/SplitManifesto";
-import TechSpecs from "@components/sections/Techspecs";
+import TechSpecs from "@components/sections/TechSpecs";
 import ProductShowcase from "@components/sections/ProductShowcase";
 import Lookbook from "@components/sections/Lookbook";
 import TechnicalIntelligence from "@components/sections/TechnicalIntelligence";
